f7ce7e5a505a5087fae475519a7a77a8caee182f
aibearde <aibearde@iupui.edu>
2018-04-29 22:04:30 -0400

    Added comments to .js and .html files

--- Ball_Movement_Damping.js
+++ Ball_Movement_Damping.js
@@ -1,1 +1,10 @@
-Ball_Movement_Damping
+// Author: Austin Bearden
+// Date Created: 4/29/2018
+// Description: Damping Experimentation for 3D Ball Movement
+// Organization: PhysicsFoosball
+// Filename: Ball_Movement_Damping.js
+
+
+console.log("Hello world!");
+
+
