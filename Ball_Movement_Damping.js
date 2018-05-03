1093a0c7a271d1e5f8d4080bb7854f82f3056a1c
aibearde <aibearde@iupui.edu>
2018-05-03 16:57:36 -0400

    Got working physics world and created sphere inside the world. Now I am experimenting with linear damping.

--- Ball_Movement_Damping.js
+++ Ball_Movement_Damping.js
@@ -4,7 +4,38 @@
 // Organization: PhysicsFoosball
 // Filename: Ball_Movement_Damping.js
 
+// build the world
+var world = new CANNON.World();
+world.gravity.set(0, 0, -9.82); // m/s^2
 
-console.log("Hello world!");
+// build a ball object
+// give the ball a shape
+var radius = 1;
+var ball = new CANNON.Body({
+    mass: 5,
+    position: new CANNON.Vec3(0, 0, 0),
+    shape: new CANNON.Sphere(radius),
+    velocity: new CANNON.Vec3(24, 0, 0)
+
+});
+// add the ball to my world
+world.addBody(ball);
+
+// create the plane for the world
+var ground = new CANNON.Body({
+    mass: 0 // mass == 0 makes body static
+            // but what does it mean to have a Static body
+});
+var ground_shape = new CANNON.Plane();
+// define the plane's shape
+ground.addShape(ground_shape);
+// add plane to world
+world.addBody(ground);
+
+// now lets figure out time!
+for(i = 0; i <= 5; i++) {
+    world.step(1/4);
+    console.log("Sphere x position: " + ball.position.x);
+}
 
 
